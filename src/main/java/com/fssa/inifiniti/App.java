33b471d340c50abef935a9dd5a13a6326c6e9370
Muthuselvam-freshworks <116705334+Muthuselvam-freshworks@users.noreply.github.com>
2023-08-16 12:03:03 +0530

    Update App.java

--- src/main/java/com/fssa/inifiniti/App.java
+++ src/main/java/com/fssa/inifiniti/App.java
@@ -4,27 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 
 public class App {
 	
 	public static Connection getConnection() throws SQLException{
 
 		Connection connect = null ;
-		String dbUrl;
-		String dbUser;
-		String dbPassword;
-
-		if (System.getenv("CI") != null) {
-			dbUrl = System.getenv("dbUrl");
-			dbUser = System.getenv("dbUser");
-			dbPassword = System.getenv("dbPassword");
-		} else {
-			Dotenv env = Dotenv.load();
-			dbUrl = env.get("dbUrl");
-			dbUser = env.get("dbUser");
-			dbPassword = env.get("dbPassword");
-		}
+//		String dbUrl;
+//		String dbUser;
+//		String dbPassword;
+//
+//		if (System.getenv("CI") != null) {
+//			dbUrl = System.getenv("dbUrl");
+//			dbUser = System.getenv("dbUser");
+//			dbPassword = System.getenv("dbPassword");
+//		} else {
+//			Dotenv env = Dotenv.load();
+//			dbUrl = env.get("dbUrl");
+//			dbUser = env.get("dbUser");
+//			dbPassword = env.get("dbPassword");
+//		}
         try {
             connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/inifiniti","root", "123456");
         } catch (Exception e) {
