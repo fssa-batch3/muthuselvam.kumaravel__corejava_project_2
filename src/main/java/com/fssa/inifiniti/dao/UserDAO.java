2dbac6265ac31f3bba3482a12b7139b943cb1744
Muthuselvam-freshworks <116705334+Muthuselvam-freshworks@users.noreply.github.com>
2023-08-31 14:54:12 +0530

    Update UserDAO.java

--- src/main/java/com/fssa/inifiniti/dao/UserDAO.java
+++ src/main/java/com/fssa/inifiniti/dao/UserDAO.java
@@ -13,7 +13,7 @@ import com.fssa.inifiniti.validationexceptions.InvalidUserException;
 public class UserDAO {
 
 	
-	
+	private static final String query = "SELECT * FROM user WHERE email=?";
 
 	
 	public boolean insertUser(User user) throws DaoException {
@@ -35,7 +35,7 @@ public class UserDAO {
 	
 	public  User findUserByEmail(String email) throws  DaoException {
 		User user = new User();
-		String insertQuery = "SELECT * FROM user WHERE email=?";
+		String insertQuery = query;
 		try (
 			
 		Connection connection = App.getConnection();
@@ -56,7 +56,7 @@ public class UserDAO {
 	
 	public  User findUserNameByEmail(String email) throws  DaoException {
 		User user = new User();
-		String insertQuery = "SELECT * FROM user WHERE email=?";
+		String insertQuery = query;
 		try (
 			
 		Connection connection = App.getConnection();
@@ -75,7 +75,7 @@ public class UserDAO {
 	
 	
 	public  boolean emailAlreadyExists(String email) throws  InvalidUserException {
-		String insertQuery = "SELECT * FROM user WHERE email=?";
+		String insertQuery = query;
 		try (
 		Connection connection = App.getConnection();
 		PreparedStatement pst = connection.prepareStatement(insertQuery)){
