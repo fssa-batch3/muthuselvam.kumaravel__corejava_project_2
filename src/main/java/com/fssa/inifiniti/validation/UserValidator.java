0e069e40bc0c05a2d6d5d954e03317f928ca6760
Muthuselvam-freshworks <116705334+Muthuselvam-freshworks@users.noreply.github.com>
2023-09-07 17:48:21 +0530

    Update UserValidator.java

--- src/main/java/com/fssa/inifiniti/validation/UserValidator.java
+++ src/main/java/com/fssa/inifiniti/validation/UserValidator.java
@@ -15,41 +15,87 @@ import com.fssa.inifiniti.validationexceptions.ValidationException;
 public class UserValidator {
 	
 	/**
-     * Validates a user's login credentials, including email and password.
-     *
-     * @param email    The email address to be validated.
-     * @param password The password to be validated.
-     * @return True if the login credentials are valid; otherwise, false.
-     * @throws ValidationException If the login details are invalid.
-     */
+	 * Validates the login credentials of a user.
+	 *
+	 * @param email The user's email address.
+	 * @param password The user's password.
+	 * @return true if both email and password are valid, otherwise false.
+	 * @throws ValidationException If either the email or password is invalid.
+	 */
 	
 	public static boolean validateLoginUser(String email, String password) throws ValidationException {
-		if( email!=null&& password!=null&&validateEmail(email)&&validatePassword(password)) {	
-			return true;
-		} else {
-			
-			throw new ValidationException("Login details are invalid");
-		}
+		
+		validateEmailNotNull(email);
+		validatePasswordNotNull(password);
+		validateEmail(email);
+		validatePassword(password);
+		
+		return true;
 	}
 	
-	  /**
-     * Validates a user, checking that the user's name, email, and password are valid.
-     *
-     * @param user The user to be validated.
-     * @return True if the user is valid; otherwise, false.
-     * @throws ValidationException If the user details are invalid.
-     */
+	/**
+	 * Validates the attributes of a User object.
+	 *
+	 * @param user The User object to validate.
+	 * @return true if all attributes of the User object are valid, otherwise false.
+	 * @throws ValidationException If any attribute of the User object is invalid.
+	 */
 	
 	public static boolean validateUser(User user) throws ValidationException {
+	
 		
-		if( user!=null&&validateName(user.getUserName())&&validateEmail(user.getEmail())&&validatePassword(user.getPassword())) {
-			
-			return true;
-		} else {
-			
-			throw new ValidationException("User details are invalid");
+		validateUserNotNull(user);
+		validateName(user.getUserName());
+		validateEmail(user.getEmail());
+		validatePassword(user.getPassword());
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Validates that the email is not null.
+	 *
+	 * @param email The email to be validated.
+	 * @return true if the email is not null, otherwise false.
+	 * @throws ValidationException If the email is null.
+	 */
+	
+	public static boolean validateEmailNotNull(String email) throws ValidationException {
+		if(email == null) {
+			throw new ValidationException("Email cannot be null");
 		}
-		
+		return true;
+	}
+	
+	/**
+	 * Validates that the password is not null.
+	 *
+	 * @param password The password to be validated.
+	 * @return true if the password is not null, otherwise false.
+	 * @throws ValidationException If the password is null.
+	 */
+	
+	public static boolean validatePasswordNotNull(String password) throws ValidationException {
+		if(password == null) {
+			throw new ValidationException("password cannot be null");
+		}
+		return true;
+	}
+	
+	/**
+	 * Validates that the user is not null.
+	 *
+	 * @param user The user to be validated.
+	 * @return true if the user is not null, otherwise false.
+	 * @throws ValidationException If the user is null.
+	 */
+	
+	public static boolean validateUserNotNull(User user) throws ValidationException {
+		if(user == null) {
+			throw new ValidationException("User is null");
+		}
+		return true;
 	}
 	
 	 /**
@@ -59,6 +105,7 @@ public class UserValidator {
      * @return True if the name is valid; otherwise, false.
      * @throws ValidationException If the name contains characters other than letters.
      */
+
 	
 	public static  boolean validateName(String name) throws ValidationException {
 			String regex = "^[A-Za-z]+$";
@@ -120,7 +167,7 @@ public class UserValidator {
      * @throws InvalidUserException If the email address is not valid.
      */
 	
-	public static boolean validateEmailId(String email) throws ValidationException {
+	public boolean validateEmailId(String email) throws ValidationException {
 	    if (email != null) {
 	        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
 	        Pattern pattern = Pattern.compile(regex);
