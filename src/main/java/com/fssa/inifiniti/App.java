c911ac12fd60fe13a1f7b83712ba809033c1df82
Muthuselvam-freshworks <116705334+Muthuselvam-freshworks@users.noreply.github.com>
2023-09-07 16:45:06 +0530

    initial commit

--- src/main/java/com/fssa/inifiniti/App.java
+++ src/main/java/com/fssa/inifiniti/App.java
@@ -5,20 +5,30 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
-
-public class App {
+/**
+ * The App class provides a method to establish a connection to the MySQL database.
+ */
+public class App { 
+	
+	/**
+	 * Establishes a connection to the MySQL database.
+	 *
+	 * @return A Connection object representing the database connection.
+	 * @throws SQLException If an SQL error occurs during the connection attempt.
+	 */
 	
 	public static Connection getConnection() throws SQLException{
 
 		Connection connect = null ;	
-        
-        String url = "jdbc:mysql://localhost:3306/inifiniti"; 
-        String user = "root";
-        String password = "123456"; 
+      
+     
+     		final String DB_URL = System.getenv("DB_URL_1");
+     		final String DB_USER = System.getenv("DB_USER_1");
+     		final String DB_PASSWORD = System.getenv("DB_PASSWORD_1");
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-             connect = DriverManager.getConnection(url, user, password);
+             connect = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
         }  catch (SQLException | ClassNotFoundException e) {
         	 e.printStackTrace();
           throw new SQLException("Unable to connect to the database");
