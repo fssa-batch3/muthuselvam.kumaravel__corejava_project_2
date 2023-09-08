186317b98048972af036d2a3a32cd2d548335c64
Muthuselvam-freshworks <116705334+Muthuselvam-freshworks@users.noreply.github.com>
2023-09-08 07:42:55 +0530

    commit changes

--- src/test/java/com/fssa/inifiniti/validation/TestValidatePassword.java
+++ src/test/java/com/fssa/inifiniti/validation/TestValidatePassword.java
@@ -5,65 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.fssa.inifiniti.validationexceptions.ValidationException;
 
 class TestValidatePassword {
 Throwable exception;
-	@Test
-	
-	void testValidPassword(){
-		
-		
-			try {
-				assertTrue(UserValidator.validatePassword("Muthu@143"));
-			} catch (ValidationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	
-	}
-	 
 
-	@Test
-	
-	 void testInvalidPasswordWithoutSpecChar(){
-		
-		
-			
-			 exception = assertThrows(ValidationException.class, () -> UserValidator.validatePassword("Muthu143"));
-				assertEquals("Password must contain one uppercase , one lowercase , one special character , length of eight character", exception.getMessage());
+@ParameterizedTest
+@ValueSource(strings = {"Muthu@143", "Muthu143","  ","Muthu@","muthu@143","MUTHU@143","muthu 143"})
+void testValidatePassword(String password) {
+    try {
+        UserValidator.validatePassword(password);
+    } catch (ValidationException e) {
+    	assertEquals("Password must contain one uppercase , one lowercase , one special character , length of eight character", e.getMessage());
 	}
-	
-	@Test
-	
-	 void testInvalidPasswordWithoutNum()  {
-		
-		
-			
-			 exception = assertThrows(ValidationException.class, () -> UserValidator.validatePassword("Muthu@"));
-				assertEquals("Password must contain one uppercase , one lowercase , one special character , length of eight character", exception.getMessage());
-	}
-	
-	@Test
-	
-	 void testInvalidPasswordWithoutCap() {
-		
-		
-			
-			 exception = assertThrows(ValidationException.class, () -> UserValidator.validatePassword("muthu@143"));
-				assertEquals("Password must contain one uppercase , one lowercase , one special character , length of eight character", exception.getMessage());
-		
-	}
-
-	@Test
-
-	 void testInvalidPasswordWithoutsmall() {
-	
-	
-	
-		 exception = assertThrows(ValidationException.class, () -> UserValidator.validatePassword("MUTHU@143"));
-			assertEquals("Password must contain one uppercase , one lowercase , one special character , length of eight character", exception.getMessage());
-	
+       
+    }
 }
-}
+
